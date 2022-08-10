4e6a88f707c5b56dd0538fd31f8b6374348edfb0
ciaralynch72 <Ciaralynch72@gmail.com>
2022-08-10 02:56:23 +0000

    Add colorama

--- wordle.py
+++ wordle.py
@@ -1,62 +1,60 @@
 
 import random
 from words import words
-
-import string
-
-
-def get_valid_word(words):
-    word = random.choice(words)  # randomly chooses something from the list
-    while '-' in word or ' ' in word:
-        word = random.choice(words)
-
-    return word.upper()
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
 
-def hangman():
-    word = get_valid_word(words)
-    word_letters = set(word)  # letters in the word
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()  # what the user has guessed
 
-    lives = 6
+def choose_word():
+    word = random.choice(words).lower()
+    return word
 
-    # getting user input
-    while len(word_letters) > 0 and lives > 0:
-        # letters used
-        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-        # what current word is (ie W - R D)
-        word_list = [letter if letter in used_letters else '-' for letter in word]
-        
-        print('Current word: ', ' '.join(word_list))
+def word_in_progress(word, guesses):
+    word_in_progress = ""
+    for letter in word:
+        if letter in guesses:
+            word_in_progress += letter
+        else:
+            word_in_progress += "*"
+    return word_in_progress
 
-        user_letter = input('Guess a letter: ').upper()
-        if user_letter in alphabet - used_letters:
-            used_letters.add(user_letter)
-            if user_letter in word_letters:
-                word_letters.remove(user_letter)
-                print('')
+
+def main(word):
+    lettersguessed = []
+    chances = 6
+    print(Fore.CYAN + "You are looking for a word that is " + str(len(word)) + " letters long.")
+
+    while True:
+        if chances != 0:
+            print("\nYou have " + str(chances) + " chances left.")
+            print("Word so far: " + word_in_progress(word, lettersguessed))
+            print(Fore.LIGHTGREEN_EX + "Letters guessed: " + str(lettersguessed))
+            guess = input("Guess: ").lower()[0]
+
+            if guess not in lettersguessed:
+                lettersguessed.append(guess)
+
+            if word_in_progress(word, lettersguessed) == word:
+                print("\nCongratulations! You got the right word: " + word)
+                break
 
             else:
-                lives = lives - 1  # takes away a life if wrong
-                print('\nYour letter,', user_letter, 'is not in the word.')
-
-        elif user_letter in used_letters:
-            print('\nYou have already used that letter. Guess another letter.')
-
+                chances -= 1
+                if guess in word:
+                    print("Correct letter!")
+                else:
+                    print(guess + " is not in the word.")
         else:
-            print('\nThat is not a valid letter.')
-
-    # gets here when len(word_letters) == 0 OR when lives == 0
-    if lives == 0:
-     
-        print('You died, sorry. The word was', word)
-    else:
-        print('YAY! You guessed the word', word, '!!')
+            print("\nOops you ran out of guesses. The correct word was " + word)
+            break
 
 
-if __name__ == '__main__':
-    hangman()
+while True:
+    word = choose_word()
+    main(word)
+    if input("Would you like to continue: ").lower().startswith("n"):
+        break
 
